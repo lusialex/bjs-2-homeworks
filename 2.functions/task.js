ba6f6bae03015c7e9e367aa36f1fb77dcfecdb7f
Lusine <eparis@atlassian.com>
2023-05-12 16:40:42 +0400

    Первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,30 @@
 function getArrayParams(...arr) {
+  let min, max, sum, avg;
 
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+    avg = +(sum / arr.length).toFixed(2);
+  }
   return { min: min, max: max, avg: avg };
 }
+getArrayParams([-99, 99, 10]);
+getArrayParams([1, 2, 3, -100, 10]);
+getArrayParams([5]);
+
+
+
+
+
 
 function summElementsWorker(...arr) {
 
@@ -19,6 +42,6 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
 }
